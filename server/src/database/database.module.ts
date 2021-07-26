578d56a9e29f82b3f8ca506e38ac09d30de5187b
jtrenaud1s <jtrenaud1s@semo.edu>
2021-07-26 00:04:17 -0500

    setup dockerfiles

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,17 +1,22 @@
-import { Connection } from "typeorm"
-import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
+import { Module } from '@nestjs/common';
+import { Connection, getConnectionOptions } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TypeOrmModule.forRoot()],
-    exports: []
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(
+          await getConnectionOptions(
+            process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+          ),
+        ),
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
-
 export class DatabaseModule {
-    constructor(private connection: Connection) {
-        if(connection.isConnected) {
-            console.log("DB connected successfully")
-        }
-
-    }
+  constructor(connection: Connection) {
+    if (connection.isConnected) console.log('DB Connected Successfully!');
+  }
 }
